adec41908198ebea29a15801e51ba714df4a8f64
Tayler Nielsen <tanielse@uci.edu>
2019-10-08 12:39:16 -0700

    Switching to class-based components
    Changed all function based components to be class-based with Typescript annotation for consistency in the project

--- template/src/components/header/index.tsx
+++ template/src/components/header/index.tsx
@@ -1,17 +1,23 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import * as style from './style.scss';
-
-
-const Header = () => (
-	<header class={style.header}>
-		<h1>Preact App</h1>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
-	</header>
-);
-
-export default Header;
+export default class Header extends Component {
+	public render() {
+		return (
+			<header class={style.header}>
+				<h1>Preact App</h1>
+				<nav>
+					<Link activeClassName={style.active} href="/">
+						Home
+                    </Link>
+					<Link activeClassName={style.active} href="/profile">
+						Me
+                    </Link>
+					<Link activeClassName={style.active} href="/profile/john">
+						John
+                    </Link>
+				</nav>
+			</header>
+		);
+	}
+}
--- template/src/routes/home/index.tsx
+++ template/src/routes/home/index.tsx
@@ -1,11 +1,14 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import * as style from './style.scss';
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
-
-export default Home;
+interface Props {}
+export default class Home extends Component<Props> {
+	public render() {
+		return (
+			<div class={style.home}>
+				<h1>Home</h1>
+				<p>This is the Home component.</p>
+			</div>
+		);
+	} 
+}
